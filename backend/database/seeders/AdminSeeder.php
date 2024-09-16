28741df3663d89c22f7edc34cf01031dcc20c5fb
Ranusha Senesh Akmeemana <seneshak2001@gmail.com>
2024-09-17 01:24:07 +0530

    Added a new column to users migration. And added admin seed

---
+++ backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin@123'), // Always hash the password
+            'voter_id' => 'ADM00000001',
+            'role' => 'admin', // Set role to 'admin'
+            'nic' => '',
+            'address' => '',
+            'district' => '',
+            'constituency' => '',
+            'theme' => 'dark', // Optionally set a theme for admin
+        ]);
+    }
+}
