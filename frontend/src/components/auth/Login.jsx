cb8f648548eb56e251c71fb33aa5c7bfb99df260
Senesh Akmeemana <seneshak2001@gmail.com>
2026-01-22 09:55:43 +0530

    Installed three dependancies. Added top navigation for register and login page.

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import AppNavbar from "../navbar/AppNavbar";
 import { useNavigate } from "react-router-dom";
 
 const Login = ({ setUserRole }) => {
@@ -61,68 +62,72 @@ const Login = ({ setUserRole }) => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10">
-      <h1 className="mb-5 text-2xl font-bold text-center">Login</h1>
-      {message && (
-        <div
-          className={`p-4 mb-4 text-sm text-center rounded ${
-            isError ? "bg-red-200 text-red-400" : "bg-green-200 text-green-400"
-          }`}
-          role="alert"
-        >
-          {message}
-        </div>
-      )}
-      <form onSubmit={handleSubmit} className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
-        <div className="mb-4">
-          <label className="block mb-2 text-sm text-left text-gray-700" htmlFor="email">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            disabled={loading} // Disable input when loading
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2 text-sm text-left text-gray-700" htmlFor="password">
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            disabled={loading} // Disable input when loading
-          />
-        </div>
-        <div className="flex items-center justify-between mb-4">
-          <label className="inline-flex items-center">
-            <input
-              type="checkbox"
-              checked={rememberMe}
-              onChange={(e) => setRememberMe(e.target.checked)}
-              className="mr-2 leading-tight"
-              disabled={loading} // Disable checkbox when loading
-            />
-            <span className="text-sm text-gray-700">Remember Me</span>
-          </label>
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            type="submit"
-            className="w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-            disabled={loading} // Disable button when loading
+    <>
+      <AppNavbar />
+
+      <div className="max-w-md mx-auto mt-10">
+        <h1 className="mb-5 text-2xl font-bold text-center">Login</h1>
+        {message && (
+          <div
+            className={`p-4 mb-4 text-sm text-center rounded ${
+              isError ? "bg-red-200 text-red-400" : "bg-green-200 text-green-400"
+            }`}
+            role="alert"
           >
-            {loading ? "Processing..." : "Login"} {/* Show "Processing..." when loading */}
-          </button>
-        </div>
-      </form>
-    </div>
+            {message}
+          </div>
+        )}
+        <form onSubmit={handleSubmit} className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
+          <div className="mb-4">
+            <label className="block mb-1 text-sm text-left text-gray-700" htmlFor="email">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              disabled={loading} // Disable input when loading
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-1 text-sm text-left text-gray-700" htmlFor="password">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              disabled={loading} // Disable input when loading
+            />
+          </div>
+          <div className="flex items-center justify-between mb-4">
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                checked={rememberMe}
+                onChange={(e) => setRememberMe(e.target.checked)}
+                className="mr-2 leading-tight"
+                disabled={loading} // Disable checkbox when loading
+              />
+              <span className="text-sm text-gray-700">Remember Me</span>
+            </label>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+              disabled={loading} // Disable button when loading
+            >
+              {loading ? "Processing..." : "Login"} {/* Show "Processing..." when loading */}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
